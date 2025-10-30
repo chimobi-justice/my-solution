9bc032b3614ee0aa6f38aa0121c687df0414169d
justice chimobi <chimobi.justice@gmail.com>
2025-10-30 22:17:30 +0100

    feat: implement product search and category filtering with mock data and product card

--- data/products.ts
+++ data/products.ts
@@ -7,11 +7,60 @@ import type { Product } from "@/types/product"
 export const products: Product[] = [
   // TODO: Add your mock products here
   // Example:
-  // {
-  //   id: 1,
-  //   name: "Wireless Headphones",
-  //   category: "Electronics",
-  //   price: 79.99,
-  //   inStock: true
-  // },
+  {
+    id: 1,
+    name: "Wireless Headphones",
+    category: "Electronics",
+    price: 79.99,
+    inStock: true
+  },
+  {
+    id: 2,
+    name: "Smartphones",
+    category: "Electronics",
+    price: 49.99,
+    inStock: true
+  },
+  {
+    id: 3,
+    name: "Headsets",
+    category: "Electronics",
+    price: 89.99,
+    inStock: true
+  },
+  {
+    id: 4,
+    name: "Television",
+    category: "Electronics",
+    price: 29.99,
+    inStock: true
+  },
+  {
+    id: 5,
+    name: "React fundamentals",
+    category: "Books",
+    price: 89.99,
+    inStock: true
+  },
+  {
+    id: 6,
+    name: "Laptop Charger",
+    category: "Electronics",
+    price: 99.99,
+    inStock: true
+  },
+  {
+    id: 7,
+    name: "T shirt",
+    category: "clothing",
+    price: 19.99,
+    inStock: true
+  },
+  {
+    id: 8,
+    name: "Hoodie",
+    category: "clothing",
+    price: 79.09,
+    inStock: true
+  },
 ]
